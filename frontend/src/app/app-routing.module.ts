206efa065b8ea50bc83a43c7e8e15ac7d7e95ac4
nhatanhhelios <62170328+nhatanhhelios@users.noreply.github.com>
2021-02-20 17:13:40 +0700

    ok

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+const routes: Routes = [{ path: 'upload', component: FileUploadComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
