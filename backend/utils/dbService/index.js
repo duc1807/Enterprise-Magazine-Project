3331293af4340997e2edf337b07166566e6d9394
Tuấn Anh Nguyễn <anhntgch18802@fpt.edu.vn>
2021-03-09 15:48:51 +0700

    Update article comment

--- backend/utils/dbService/index.js
+++ backend/utils/dbService/index.js
@@ -1,34 +1,44 @@
-const { getEventsByFacultyName, createNewEvent, updateEvent, deleteEventById } = require('../../utils/dbService/eventService')
-const { getAllFaculty, getFacultyById } = require('../../utils/dbService/facultyService')
-const { getPostedArticlesOfEvent } = require('../../utils/dbService/articleService')
-const { getCoordinatorAccountsByFaculty } = require('../../utils/dbService/coordinatorService')
-const { getStudentAccountByFaculty } = require('../../utils/dbService/studentService')
-const { getAccountByEmail } = require('../../utils/dbService/accountService')
-const { getImageById } = require('../../utils/dbService/imageService')
+const {
+	getEventsByFacultyName,
+	createNewEvent,
+	updateEvent,
+	deleteEventById,
+} = require('../../utils/dbService/eventService');
+const { getAllFaculty, getFacultyById } = require('../../utils/dbService/facultyService');
+const {
+	getPostedArticlesOfEvent,
+	getSubmittedArticleById,
+	addNewCommentToArticle,
+} = require('../../utils/dbService/articleService');
+const { getCoordinatorAccountsByFaculty } = require('../../utils/dbService/coordinatorService');
+const { getStudentAccountByFaculty } = require('../../utils/dbService/studentService');
+const { getAccountByEmail } = require('../../utils/dbService/accountService');
+const { getImageById } = require('../../utils/dbService/imageService');
 
 module.exports = {
-    // Account
-    getAccountByEmail: getAccountByEmail,
+	// Account
+	getAccountByEmail: getAccountByEmail,
 
-    // Faculty
-    getAllFaculty: getAllFaculty,
-    getFacultyById: getFacultyById,
+	// Faculty
+	getAllFaculty: getAllFaculty,
+	getFacultyById: getFacultyById,
 
-    // Event
-    getEventsByFacultyName: getEventsByFacultyName,
-    createNewEvent: createNewEvent,
-    updateEvent: updateEvent,
-    deleteEventById: deleteEventById,
+	// Event
+	getEventsByFacultyName: getEventsByFacultyName,
+	createNewEvent: createNewEvent,
+	updateEvent: updateEvent,
+	deleteEventById: deleteEventById,
 
-    // Article
-    getPostedArticlesOfEvent: getPostedArticlesOfEvent,
+	// Article
+	getPostedArticlesOfEvent: getPostedArticlesOfEvent,
+	getSubmittedArticleById: getSubmittedArticleById,
+	addNewCommentToArticle: addNewCommentToArticle,
+	// Coordinator Manager
+	getCoordinatorAccountsByFaculty: getCoordinatorAccountsByFaculty,
 
-    // Coordinator Manager
-    getCoordinatorAccountsByFaculty: getCoordinatorAccountsByFaculty,
+	// Student
+	getStudentAccountByFaculty: getStudentAccountByFaculty,
 
-    // Student
-    getStudentAccountByFaculty: getStudentAccountByFaculty,
-
-    // Image
-    getImageById: getImageById,
-}
+	// Image
+	getImageById: getImageById,
+};
