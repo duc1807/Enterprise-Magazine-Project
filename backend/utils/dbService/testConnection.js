b71e20b232280e5d1b1c826bff0ba1820f6edcc6
duc1807 <62170104+duc1807@users.noreply.github.com>
2021-03-09 16:01:43 +0700

    Update test changes

---
+++ backend/utils/dbService/testConnection.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql2");
+
+const dbconfig = {
+  host: "45.252.250.39",
+  user: "ucifbahh_gw",
+  password: "tvXTdE[[uO$M",
+  database: "ucifbahh_MagazineDB",
+  port: 3306,
+  multipleStatements: true,
+  connectionLimit: 50,
+  queueLimit: 20,
+};
+
+// dbconfig = {
+//   host: "sql6.freemysqlhosting.net",
+//   user: "sql6397094",
+//   password: "QBAyQkR9HU",
+//   database: "sql6397094",
+//   port: 3306,
+//   multipleStatements: true,
+//   connectionLimit: 10,
+//   queueLimit: 20,
+//   // waitForConnections: false,
+// }
+
+const connection = mysql.createConnection(dbconfig);
+
+connection.connect(function (err) {
+  if (!!err) console.log(err);
+  else console.log("Database connected");
+});
