4e3b8f640883f0d327440838f9329a5c573b32a0
duc1807 <62170104+duc1807@users.noreply.github.com>
2021-03-12 15:11:12 +0700

    ✔ Mock-up coordinator post article API [✔]

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const {
-	getSubmittedArticleById,
-	addNewCommentToArticle,
-	setSelectedArticle,
-	getEventByArticleId,
+  getSubmittedArticleById,
+  addNewCommentToArticle,
+  setSelectedArticle,
+  getEventByArticleId,
 } = require("../utils/dbService/index");
 const { insertFolderToOtherFolder } = require("../utils/driveAPI");
 const { gwAccountValidation } = require("./middleware/verification");
@@ -22,143 +22,214 @@ const _COORDINATOR_PERMISSION_ID = 2;
  * @notes
  *      - (!!! CORS problems)
  */
-router.post("/:articleId/add-comment", gwAccountValidation, async (req, res) => {
-	// Get comment content
-	const { content } = req.body;
-	// Get articleId from params
-	const { articleId } = req.params;
+router.post(
+  "/:articleId/add-comment",
+  gwAccountValidation,
+  async (req, res) => {
+    // Get comment content
+    const { content } = req.body;
+    // Get articleId from params
+    const { articleId } = req.params;
 
-	// Get userInfo passed from middleware
-	const data = res.locals.data;
+    // Get userInfo passed from middleware
+    const data = res.locals.data;
 
-	// Check permission is coordinator or not
-	if (data.userInfo.FK_role_id != _COORDINATOR_PERMISSION_ID) {
-		return res.status(401).json({
-			status: res.statusCode,
-			success: false,
-			message: "Permission required!",
-		});
-	}
+    // Check permission is coordinator or not
+    if (data.userInfo.FK_role_id != _COORDINATOR_PERMISSION_ID) {
+      return res.status(401).json({
+        status: res.statusCode,
+        success: false,
+        message: "Permission required!",
+      });
+    }
 
-	// Log the data for testing
-	console.log("data: ", data);
+    // Log the data for testing
+    console.log("data: ", data);
 
-	// Get the current time
-	const currentTime = new Date();
+    // Get the current time
+    const currentTime = new Date();
 
-	// Create comment data to INSERT into database
-	const commentData = {
-		content: content,
-		time: currentTime.getTime(),
-		FK_article_id: articleId,
-		FK_coordinator_id: data.userInfo.account_id,
-	};
+    // Create comment data to INSERT into database
+    const commentData = {
+      content: content,
+      time: currentTime.getTime(),
+      FK_article_id: articleId,
+      FK_coordinator_id: data.userInfo.account_id,
+    };
 
-	// Await database query
-	await addNewCommentToArticle(commentData, data.userInfo)
-		.then((result) => {
-			return res.status(201).json({
-				status: res.statusCode,
-				success: true,
-				commentInfo: commentData,
-			});
-		})
-		.catch((err) => {
-			if (!!err) {
-				console.log("Err: ", err);
-				return res.status(501).json({
-					status: res.statusCode,
-					success: false,
-					messages: "Bad request",
-				});
-			} else {
-				// If article doesnt exist or permission invalid
-				return res.status(401).json({
-					status: res.statusCode,
-					success: false,
-					message: "Invalid request!",
-				});
-			}
-		});
-});
+    // Await database query
+    await addNewCommentToArticle(commentData, data.userInfo)
+      .then((result) => {
+        return res.status(201).json({
+          status: res.statusCode,
+          success: true,
+          commentInfo: commentData,
+        });
+      })
+      .catch((err) => {
+        if (!!err) {
+          console.log("Err: ", err);
+          return res.status(501).json({
+            status: res.statusCode,
+            success: false,
+            messages: "Bad request",
+          });
+        } else {
+          // If article doesnt exist or permission invalid
+          return res.status(401).json({
+            status: res.statusCode,
+            success: false,
+            message: "Invalid request!",
+          });
+        }
+      });
+  }
+);
 
 /**
  * @method POST
  * @API api/article/:articleId/select-article
- * @description API for select article submission to the event's selected article
+ * @description API for select article submission to the folder event's selected article
  * @params
  * 		- articleId: Int
  * @return null
  * @notes
  * 		- Should use method PUT ???
+ * 		- Should pass articleId in req.params ??? Or body ???
  */
 router.post(
-	"/:articleId/select-article",
-	gwAccountValidation,
-	async (req, res) => {
-		// Get articleId from param
-		const { articleId } = req.params;
-		// Get userInfo passed from middleware
-		const data = res.locals.data;
+  "/:articleId/select-article",
+  gwAccountValidation,
+  async (req, res) => {
+    // Get articleId from param
+    const { articleId } = req.params;
+    // Get userInfo passed from middleware
+    const data = res.locals.data;
 
-		// Check permission is coordinator or not
-		if (data.userInfo.FK_role_id != _COORDINATOR_PERMISSION_ID) {
-			return res.status(401).json({
-				status: res.statusCode,
-				success: false,
-				message: "Permission required!",
-			});
-		}
+    // Check permission is coordinator or not
+    if (data.userInfo.FK_role_id != _COORDINATOR_PERMISSION_ID) {
+      return res.status(401).json({
+        status: res.statusCode,
+        success: false,
+        message: "Permission required!",
+      });
+    }
 
-		// Set article status to selected in database
-		const query = setSelectedArticle(articleId);
+    // Set article status to selected in database
+    const query = setSelectedArticle(articleId);
 
-		await query
-			.then(async (result) => {
-				// If updated successfully, the get event information by articleId
-				const query1 = getEventByArticleId(articleId);
+    await query
+      .then(async (result) => {
+        // If updated successfully, the get event information by articleId
+        const query1 = getEventByArticleId(articleId);
 
-				// After getting event information successfully, then alter folder in google drive
-				await query1
-					.then((articleAndEventInfo) => {
-						console.log(articleAndEventInfo);
+        // After getting event information successfully, then alter folder in google drive
+        await query1
+          .then((articleAndEventInfo) => {
+            console.log(articleAndEventInfo);
 
-						// Asynchronous move article folder to selected article in Drive
-						insertFolderToOtherFolder(articleAndEventInfo);
+            // Asynchronous move article folder to selected article in Drive
+            insertFolderToOtherFolder(articleAndEventInfo);
 
-						// Return the success status
-						return res.status(204).json({
-							status: res.statusCode,
-							success: true,
-						});
-					})
-					.catch((err) => {
-						console.log(err);
-						return res.status(501).json({
-							status: res.statusCode,
-							success: false,
-							messages: "Bad request",
-						});
-					});
-			})
-			.catch((err) => {
-				if (!!err) {
-					console.log("Err: ", err);
-					return res.status(501).json({
-						status: res.statusCode,
-						success: false,
-						messages: "Bad request",
-					});
-				} else {
-					// If article || article status == 'pending' doesnt exist
-					return res.status(404).json({
-						status: res.statusCode,
-						success: false,
-						message: "Invalid request!",
-					});
-				}
-			});
-	}
+            // Return the success status
+            return res.status(204).json({
+              status: res.statusCode,
+              success: true,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(501).json({
+              status: res.statusCode,
+              success: false,
+              messages: "Bad request",
+            });
+          });
+      })
+      .catch((err) => {
+        if (!!err) {
+          console.log("Err: ", err);
+          return res.status(501).json({
+            status: res.statusCode,
+            success: false,
+            messages: "Bad request",
+          });
+        } else {
+          // If article || article status == 'pending' doesnt exist
+          return res.status(404).json({
+            status: res.statusCode,
+            success: false,
+            message: "Invalid request!",
+          });
+        }
+      });
+  }
 );
+
+/**
+ * @method POST
+ * @API api/article/post-article
+ * @description API for post article to event homepage
+ * @params
+ * 		- title: String
+ * 		- content: String
+ * 		- author: String (email)
+ * @return null
+ * @notes
+ */
+router.post("/post-article", gwAccountValidation, async (req, res) => {
+  // Get information from param
+  const { title, content, author } = req.params;
+  // Get userInfo passed from middleware
+  const data = res.locals.data;
+
+  // Check permission is coordinator or not
+  if (data.userInfo.FK_role_id != _COORDINATOR_PERMISSION_ID) {
+    return res.status(401).json({
+      status: res.statusCode,
+      success: false,
+      message: "Permission required!",
+    });
+  }
+
+  // Get current time
+  const currentTime = new Date()
+  // Create article Object to INSERT INTO database
+  const article = {
+	title: title,
+	content: content,
+	author: author,
+	postedDate: currentTime.getTime()
+  }
+
+  // INSERT article into 'Posted_Article' table
+  const query = postNewArticle(article);
+
+  await query
+    .then((result) => {
+		return res.status(201).json({
+			status: res.statusCode,
+			success: true,
+			message: "New article posted successfully",
+		  });
+    })
+    .catch((err) => {
+      if (!!err) {
+        console.log("Err: ", err);
+        return res.status(501).json({
+          status: res.statusCode,
+          success: false,
+          message: "Bad request",
+        });
+      } else {
+        // If event not exist || permission to event is invalid
+        return res.status(404).json({
+          status: res.statusCode,
+          success: false,
+          message: "Invalid request!",
+        });
+      }
+    });
+});
 
 module.exports = router;
